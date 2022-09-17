e821dce532511548de2d419b0dbaf8ceb726f049
katsuimasato <masato.lond2@outlook.jp>
2022-09-17 11:38:56 +0900

    fix how to output

--- extractor.js
+++ extractor.js
@@ -13,37 +13,58 @@ const $ = require('jquery')(windowObject);
 program.parse(process.argv)
 const filePath = program.args[0]
 
-let output = ``
+let devidedArray = []
 
-fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+const file = fs.readFileSync(filePath, { encoding: "utf8" }, err => {
   if (err) {
     console.error(err);
-  } else {
-    let document = parser.parseFromString(file, "text/html")
-    let h2Item = document.querySelector('h2')
-    let devidedArray = []
-
-    let siblings = Array.from(h2Item.parentNode.children).filter(element => {
-      if (element.localName === 'h2' || element.localName === 'div') return element;
-    });
-
-    // 配列内の要素を [[h2, div, div, div], [h2, div], ...] のようにh2を基準に分割する
-    siblings.reduce((acc, item, idx) => {
-      if (item.localName == 'h2' && idx != 0) {
-        devidedArray.push(acc)
-        acc = []
-      }
-      acc.push(item)
-      return acc
-    }, [])
-
-    // console.log(devidedArray[0][0].textContent)
   }
 });
 
+let document = parser.parseFromString(file, "text/html")
+let h2Item = document.querySelector('h2')
 
-// fs.writeFileSync('output.md', output, (err) => {
-//   if (err) { throw err; }
-//   console.log('made output');
-// });
+let siblings = Array.from(h2Item.parentNode.children).filter(element => {
+  if (element.localName === 'h2' || element.localName === 'div') return element;
+});
+
+
+const toTextStrings = (nodeLists) => {
+  const text = nodeLists.reduce((prev, node, idx) => {
+    // note部分
+    if (node.classList.contains('row') && node.classList.contains('with-border')) {
+      return prev.concat(`${node.textContent.replace(/\r?\n/g, '')}<br>\n\n\n`)
+    } else if (node.classList.contains('row') && !node.classList.contains('by-date')) {
+      // ハイライト部分
+      return prev.concat(`> ${node.textContent.replace(/\r?\n/g, '')}\n\n`)
+    } else if (node.localName === 'h2') {
+      // h2要素
+      return prev.concat(`\n## ${node.textContent.replace(/\r?\n/g, '')}\n\n`)
+    } else {
+      // TODO: ページ部分の対応について
+      return prev
+    }
+  }, '')
+  return text
+}
+
+// 配列内の要素を [[h2, div, div, div], [h2, div], ...] のようにh2を基準に分割する
+siblings.reduce((acc, item, idx) => {
+  if (item.localName === 'h2' && idx != 0) {
+    devidedArray.push(acc)
+    acc = []
+  }
+  acc.push(item)
+  return acc
+}, [])
+
+let output = devidedArray.reduce((prev, elements) => {
+  return prev.concat(toTextStrings(elements))
+}, '')
+
+
+// 出力文字列作成処理
+fs.writeFileSync('output.md', output, (err) => {
+  if (err) { throw err; }
+});
 
